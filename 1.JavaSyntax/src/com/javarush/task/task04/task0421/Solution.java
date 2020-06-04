a7755d18dcd8f94a7b0c73861f27bd432d615976
Sergey Savrukhin <savrawar@mail.ru>
2020-06-04 13:38:13 +0300

    task0421

--- 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
@@ -4,9 +4,19 @@ package com.javarush.task.task04.task0421;
 Настя или Настя?
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String firstName = bufferedReader.readLine();
+        String secondName = bufferedReader.readLine();
+
+        if(firstName.equals(secondName))
+            System.out.println("Имена идентичны");
+        else if(firstName.length() == secondName.length())
+            System.out.println("Длины имен равны");
 
     }
 }
