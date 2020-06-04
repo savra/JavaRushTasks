d75580940e85bb7c8142d2094e47439c43fc74a2
Sergey Savrukhin <savrawar@mail.ru>
2020-06-04 13:55:47 +0300

    task0424

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task04.task0424;
+
+/* 
+Три числа
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bufferedReader.readLine());
+        int b = Integer.parseInt(bufferedReader.readLine());
+        int c = Integer.parseInt(bufferedReader.readLine());
+
+        if(a == b)
+            System.out.println(3);
+        else if (a == c)
+            System.out.println(2);
+        else if (b == c)
+            System.out.println(1);
+    }
+}
