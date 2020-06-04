dfabba3ce62591fd10a51e6b71ba95466f6282b9
Sergey Savrukhin <savrawar@mail.ru>
2020-06-04 15:20:22 +0300

    task0443

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task04.task0443;
+
+/* 
+Как назвали, так назвали
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String name = bufferedReader.readLine();
+        int y = Integer.parseInt(bufferedReader.readLine());
+        int m = Integer.parseInt(bufferedReader.readLine());
+        int d = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println("Меня зовут " + name + ".");
+        System.out.println("Я родился " + d + "." + m + "." + y);
+
+    }
+}
