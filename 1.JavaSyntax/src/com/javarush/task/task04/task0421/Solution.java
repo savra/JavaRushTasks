b6c914d530f5bd1290e53d0b231deab9392b2dc7
Sergey Savrukhin <savrawar@mail.ru>
2020-06-04 13:35:19 +0300

    task0421

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task04.task0421;
+
+/* 
+Настя или Настя?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+
+    }
+}
