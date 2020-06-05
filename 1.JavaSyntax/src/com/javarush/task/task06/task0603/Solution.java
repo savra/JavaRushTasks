784920d409ca7496cefcb6a146257e8cacba99ce
Sergey Savrukhin <savrawar@mail.ru>
2020-06-05 15:05:31 +0300

    task0603

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0603/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task06.task0603;
+
+/* 
+По 50 000 объектов Cat и Dog
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Cat cat;
+        Dog dog;
+        for(int i = 0; i < 100000; i++) {
+            if(i <= 49999) {
+                cat = new Cat();
+                cat = null;
+            }
+            else {
+                dog = new Dog();
+                dog = null;
+            }
+
+        }
+    }
+}
+
+class Cat {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("A Cat was destroyed");
+    }
+}
+
+class Dog {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("A Dog was destroyed");
+    }
+}
