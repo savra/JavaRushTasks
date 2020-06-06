32a70fc1de4edb78404585579804dfb0a46919ad
Sergey Savrukhin <savrawar@mail.ru>
2020-06-06 12:56:30 +0300

    task0306

--- 1.JavaSyntax/src/com/javarush/task/task03/task0306/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0306/Solution.java
@@ -6,6 +6,6 @@ package com.javarush.task.task03.task0306;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println((2 * 3) + 4 * 5 + (6 * 7));
+        System.out.println(2 * (3 + 4 * (5 + 6 * 7)));
     }
 }
