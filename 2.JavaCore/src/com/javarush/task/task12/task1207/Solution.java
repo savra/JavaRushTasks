0d45ca58071ba8a56c88196b9a38d8427c9c5236
Sergey Savrukhin <savrawar@mail.ru>
2020-06-09 20:35:57 +0300

    task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer i1 = 5;
+        int i2 = 6;
+
+        print(i1);
+        print(i2);
+    }
+
+    public static void print(int v) {
+        System.out.println(v);
+    }
+
+    public static void print(Integer v) {
+        System.out.println(v);
+    }
+}
