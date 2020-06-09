121735412af8078a91515c8ac5ef6fc63c0d87bc
Sergey Savrukhin <savrawar@mail.ru>
2020-06-09 13:40:34 +0300

    task0807

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task08.task0807;
+
+/* 
+LinkedList и ArrayList
+*/
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    public static Object createArrayList() {
+        return new ArrayList<>();
+    }
+
+    public static Object createLinkedList() {
+        return new LinkedList<>();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
