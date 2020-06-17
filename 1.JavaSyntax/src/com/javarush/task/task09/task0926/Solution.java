94fd2aa879e4c350e708d9378e40838f8e4f441b
Sergey Savrukhin <savrawar@mail.ru>
2020-06-17 18:13:44 +0300

    task0926

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0926/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task09.task0926;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список из массивов чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        ArrayList<int[]> list = createList();
+        printList(list);
+    }
+
+    public static ArrayList<int[]> createList() {
+        ArrayList<int[]> arrayList = new ArrayList<>();
+        arrayList.add(new int[] {0, 1, 2, 3, 4});
+        arrayList.add(new int[] {6, 7});
+        arrayList.add(new int[] {8, 9, 10, 11});
+        arrayList.add(new int[] {12, 13, 14, 15, 16, 17, 18});
+        arrayList.add(new int[0]);
+
+        return arrayList;
+    }
+
+    public static void printList(ArrayList<int[]> list) {
+        for (int[] array : list) {
+            for (int x : array) {
+                System.out.println(x);
+            }
+        }
+    }
+}
