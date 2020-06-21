78376a30f24d1421e6bb0b7b88e05930f939e182
Sergey Savrukhin <savrawar@mail.ru>
2020-06-21 15:27:09 +0300

    task1324

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1324/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task13.task1324;
+
+/* 
+Один метод в классе
+*/
+
+import java.awt.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Animal {
+        Color getColor();
+
+        Integer getAge();
+    }
+
+    public abstract class Fox implements Animal {
+        public String getName() {
+            return "Fox";
+        }
+
+    }
+}
