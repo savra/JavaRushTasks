a0bb4bb080b4399486b647fa1e2762883295f7d9
Sergey Savrukhin <savrawar@mail.ru>
2020-06-22 11:33:29 +0300

    task1013

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1013/Solution.java
@@ -0,0 +1,57 @@
+package com.javarush.task.task10.task1013;
+
+/* 
+Конструкторы класса Human
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public static class Human {
+        private int a;
+        private double b;
+        private char c;
+        private boolean d;
+        private byte e;
+        private short f;
+
+        public Human() {
+
+        }
+
+        public Human(int a) {
+
+        }
+
+        public Human(double b) {
+
+        }
+
+        public Human(char c) {
+
+        }
+
+        public Human(boolean d) {
+
+        }
+
+        public Human(byte e) {
+
+        }
+
+        public Human(short e) {
+
+        }
+
+        public Human(byte e, short d) {
+
+        }
+        public Human(byte e, char c) {
+
+        }
+        public Human(byte e, int a) {
+
+        }
+    }
+}
