1878f7218cca2839df5623709f676c111410cb8c
Sergey Savrukhin <savrawar@mail.ru>
2020-06-25 10:13:16 +0300

    task1629

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task16.task1629;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InterruptedException {
+        Read3Strings t1 = new Read3Strings();
+        Read3Strings t2 = new Read3Strings();
+
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+
+        t1.printResult();
+        t2.printResult();
+    }
+
+    public static class Read3Strings extends Thread {
+        private String str = "";
+
+        @Override
+        public void run() {
+            int stringCount = 3;
+
+            try {
+                while (stringCount > 0) {
+                    str += reader.readLine() + " ";
+                    stringCount--;
+                }
+            } catch (IOException e) {
+
+            }
+        }
+
+        public void printResult() {
+            System.out.println(str);
+        }
+    }
+}
