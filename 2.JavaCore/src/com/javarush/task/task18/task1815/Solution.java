a13fcda849c4f606bfa6490403f8093043c9f279
Sergey Savrukhin <savrawar@mail.ru>
2020-08-24 19:49:35 +0300

    task1815

--- 2.JavaCore/src/com/javarush/task/task18/task1815/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1815/Solution.java
@@ -8,19 +8,26 @@ import java.util.List;
 
 public class Solution {
     public class TableInterfaceWrapper implements TableInterface {
+        private TableInterface tableInterface;
+
+        public TableInterfaceWrapper(TableInterface tableInterface) {
+            this.tableInterface = tableInterface;
+        }
 
         @Override
         public void setModel(List rows) {
             System.out.println(rows.size());
+            tableInterface.setModel(rows);
         }
 
         @Override
         public String getHeaderText() {
-            return null;
+            return tableInterface.getHeaderText().toUpperCase();
         }
 
         @Override
         public void setHeaderText(String newHeaderText) {
+            tableInterface.setHeaderText(newHeaderText);
         }
     }
 
