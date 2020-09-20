a2766aea7526c2926c2929f15bb5f030dbb3b35b
Sergey Savrukhin <savrawar@mail.ru>
2020-09-20 17:26:13 +0300

    task1523

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    private Solution() {
+
+    }
+
+    Solution(Object o) {
+
+    }
+
+    protected Solution(Number n) {
+
+    }
+
+    public Solution(Integer n) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution(Object o) {
+        super(o);
+    }
+
+    protected SubSolution(Number n) {
+        super(n);
+    }
+
+    SubSolution(Integer n) {
+        super(n);
+    }
+}
