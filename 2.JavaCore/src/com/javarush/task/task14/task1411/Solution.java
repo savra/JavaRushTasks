f3c3b43b55d758728a2d638fe9bb065459dd7cfe
Sergey Savrukhin <savrawar@mail.ru>
2020-10-10 19:01:27 +0300

    task1411

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User, Loser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        for(String i = reader.readLine(); "user".equals(i) || "loser".equals(i) || "coder".equals(i) || "proger".equals(i); i = reader.readLine())
+        {
+            switch (i) {
+                case "user":
+                    person = new Person.User();
+                    break;
+                case "loser":
+                    person = new Person.Loser();
+                    break;
+                case "coder":
+                    person = new Person.Coder();
+                    break;
+                case "proger":
+                    person = new Person.Proger();
+                    break;
+                default:
+                    break;
+            }
+
+            doWork(person); //вызываем doWork
+        }
+    }
+
+    public static void doWork(Person person) {
+        if (person instanceof Person.User) {
+            ((Person.User) person).live();
+        } else if (person instanceof Person.Loser) {
+            ((Person.Loser) person).doNothing();
+        } else if (person instanceof Person.Coder) {
+            ((Person.Coder) person).writeCode();
+        } else if (person instanceof Person.Proger) {
+            ((Person.Proger) person).enjoy();
+        }
+    }
+}
