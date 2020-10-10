4702823dc02f7bc2bff7cea01894939f7b32c272
Sergey Savrukhin <savrawar@mail.ru>
2020-10-10 20:12:48 +0300

    Игра сапер (5/16)

--- 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
+++ 8.Games/src/com/javarush/games/minesweeper/MinesweeperGame.java
@@ -5,11 +5,16 @@ import com.javarush.engine.cell.*;
 public class MinesweeperGame extends Game {
     private static final int SIDE = 9;
     private GameObject[][] gameField = new GameObject[SIDE][SIDE];
+    private int countMinesOnField;
 
     private void createGame() {
         for (int i = 0; i < SIDE; i++) {
             for (int j = 0; j < SIDE; j++) {
-                gameField[i][j] = new GameObject(j, i);
+                gameField[i][j] = new GameObject(j, i, (1 / (getRandomNumber(10) + 1)) == 1);
+
+                if (gameField[i][j].isMine)
+                    countMinesOnField++;
+
                 setCellColor(i, j, Color.CORNFLOWERBLUE);
             }
         }
