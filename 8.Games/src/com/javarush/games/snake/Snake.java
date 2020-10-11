11c0fb4b79d1a78f5ed11967fc8290e5ca61c9b1
Sergey Savrukhin <savrawar@mail.ru>
2020-10-11 15:46:13 +0300

    Игра "Змейка" (8/20)

--- 8.Games/src/com/javarush/games/snake/Snake.java
+++ 8.Games/src/com/javarush/games/snake/Snake.java
@@ -9,6 +9,12 @@ public class Snake {
     private static final String HEAD_SIGN = "\uD83D\uDC7E";
     private static final String BODY_SIGN = "\u26AB";
     private List<GameObject> snakeParts = new ArrayList<>();
+    private Direction direction = Direction.LEFT;
+    public boolean isAlive = true;
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
 
     public Snake(int x, int y) {
         snakeParts.add(new GameObject(x, y));
@@ -17,10 +23,12 @@ public class Snake {
     }
 
     public void draw(Game game) {
-        game.setCellValue(snakeParts.get(0).x, snakeParts.get(0).y, HEAD_SIGN);
+        Color currentColor = this.isAlive ? Color.BLACK : Color.RED;
+
+        game.setCellValueEx(snakeParts.get(0).x, snakeParts.get(0).y, Color.NONE, HEAD_SIGN, currentColor, 75);
 
         for (int i = 1; i < snakeParts.size(); i++) {
-                game.setCellValue(snakeParts.get(i).x, snakeParts.get(i).y, BODY_SIGN);
+                game.setCellValueEx(snakeParts.get(i).x, snakeParts.get(i).y, Color.NONE, BODY_SIGN, currentColor, 75);
         }
     }
 }
