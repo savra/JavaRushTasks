49a75290a72c296f2198f1d94c66310949c48b37
Sergey Savrukhin <savrawar@mail.ru>
2020-10-18 18:15:15 +0300

    Игра 2048 (4/18)

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -2,6 +2,8 @@ package com.javarush.games.game2048;
 
 import com.javarush.engine.cell.*;
 
+import java.util.Arrays;
+
 public class Game2048 extends Game {
     private static final int SIDE = 4;
     private int[][] gameField = new int[SIDE][SIDE];
@@ -10,9 +12,18 @@ public class Game2048 extends Game {
     public void initialize() {
         setScreenSize(SIDE, SIDE);
         createGame();
+        drawScene();
     }
 
     private void createGame() {
 
+    }
+
+    private void drawScene() {
+        for (int i = 0; i < gameField.length; i++) {
+            for (int j = 0; j < gameField[i].length; j++) {
+                setCellColor(i, j, Color.ALICEBLUE);
+            }
+        }
     }
 }
