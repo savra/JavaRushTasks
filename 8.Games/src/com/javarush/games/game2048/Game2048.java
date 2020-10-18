a644a10ba244ff2e492fbf7ecfd0a387c727ee83
Sergey Savrukhin <savrawar@mail.ru>
2020-10-18 18:15:32 +0300

    Игра 2048 (4/18)

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -2,8 +2,6 @@ package com.javarush.games.game2048;
 
 import com.javarush.engine.cell.*;
 
-import java.util.Arrays;
-
 public class Game2048 extends Game {
     private static final int SIDE = 4;
     private int[][] gameField = new int[SIDE][SIDE];
